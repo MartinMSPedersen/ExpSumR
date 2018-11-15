578bf313ee08d012b2e6e718cc3f1dc301aefd66
Martin Møller Skarbiniks Pedersen <traxplayer@gmail.com>
2018-11-15 18:42:51 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,18 +1,7 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyUI(fluidPage(
-
-  # Application title
   titlePanel("Exponential Sums"),
-
-  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       sliderInput("m",
@@ -32,10 +21,8 @@ shinyUI(fluidPage(
               min = 1,
               max = 99,
               value = 17)),
-
-    # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("expPlot")
     )
   )
 ))
