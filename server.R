98b472754c37c53c9eb69d409d6891fe58b5db15
Martin Møller Skarbiniks Pedersen <traxplayer@gmail.com>
2018-11-15 18:43:17 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,15 +1,8 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
+  output$exPlot <- renderPlot({
 
     library(numbers)
     library(ggplot2)
